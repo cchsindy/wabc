872a6f80eafefa9b5f538ad774df374c3c033c80
Brad Spencer <bradspencer@Brads-Mac-mini.local>
2019-09-23 15:03:36 -0400

    added firebase

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>WABC Membership</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
